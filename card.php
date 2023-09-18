2644ddb6ece1b22a3c4b18384024db56633514b2
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-18 17:33:00 -0500

    Update card.php

--- card.php
+++ card.php
@@ -1,8 +1,10 @@
 <?php
 require_once("util-db.php");
 require_once("model-card.php");
-$PageTitle = "Home";
+
+$PageTitle = "Cards";
 include "view-header.php";
+
 $cards = selectCard();
 include "view-card.php";
 include "view-footer.php";
