849767a4f35516af924f1e31c3c29b8a830930a3
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-18 17:41:49 -0500

    Update model-card.php

--- model-card.php
+++ model-card.php
@@ -2,7 +2,7 @@
 function selectCard() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT cardnumber, cardname, cardtype FROM `card` WHERE 1")
+        $stmt = $conn->prepare("SELECT cardnumber, cardname, cardtype FROM `card`")
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
