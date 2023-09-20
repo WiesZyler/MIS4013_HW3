5181c29e39ff84713911227ba9091dbe6ff51c3e
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-20 13:49:41 -0500

    Update model-pack.php

--- model-pack.php
+++ model-pack.php
@@ -2,7 +2,7 @@
 function selectPack() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT packID, packname, releasedate FROM `pack` WHERE 1");
+        $stmt = $conn->prepare("SELECT packID, packname, packType, releasedate FROM `pack` WHERE 1");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
