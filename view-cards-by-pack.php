fe876208a37eb6971286ff9c5c0fb3253109b416
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-25 14:13:55 -0500

    Update view-cards-by-pack.php

--- view-cards-by-pack.php
+++ view-cards-by-pack.php
@@ -1,5 +1,5 @@
 <h1>Cards By Pack</h1>
-<h2> <?php echo $pack['packname'];?> (<?php echo $pack['packID'];?></td><td>), <?php echo $pack['releasedate'];?></h2>
+<h2> <?php echo $pack['packname'] (echo $pack['packID']), echo $pack['releasedate'];?></h2>
 <div class="table-responsive">
   <table class="table">
     <thead>
