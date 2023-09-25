bb00b2432f550808e116143cc981f96a88a3bd6f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-25 13:54:19 -0500

    Update model-cards-by-pack.php

--- model-cards-by-pack.php
+++ model-cards-by-pack.php
@@ -3,7 +3,7 @@ function selectCardsByPack($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT P.packID, packname, packType, releasedate, cardname, cardnumber, cardtype FROM `pack` P JOIN `card` C on P.packID = C.packID where C.cardID = ?");
-        $stmt->bind_param("i", $pid);
+        $stmt->bind_param("s", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
