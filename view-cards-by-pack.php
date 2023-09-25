cb779421a0d2deb89a3848fe7fe49f65c45128fa
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-25 14:18:03 -0500

    Update view-cards-by-pack.php

--- view-cards-by-pack.php
+++ view-cards-by-pack.php
@@ -1,8 +1,10 @@
 <h1>Cards By Pack</h1>
-while($pack = $packs->fetch_assoc())
-{
+<?php while($pack = $packs->fetch_assoc())
+{?>
 <h2><?php echo $pack['packname'] . ' (' . $pack['packID'] . ') ' . $pack['releasedate']; ?></h2>
-}
+ <?php
+} 
+?>
 <div class="table-responsive">
   <table class="table">
     <thead>
