4451d293c2f9dd49867f25f62991da8796e35f21
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-25 13:49:30 -0500

    Update cards-by-pack.php

--- cards-by-pack.php
+++ cards-by-pack.php
@@ -5,7 +5,7 @@ require_once("model-cards-by-pack.php");
 $PageTitle = "Cards By Pack";
 include "view-header.php";
 
-$packs = selectCardsByPack($_GET['id']);
+$packs = selectCardsByPack($_GET["id"]);
 include "view-cards-by-pack.php";
 include "view-footer.php";
 ?>
