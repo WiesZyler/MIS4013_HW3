47f80cf2e4bdff2cae574b98afe02e1e70740973
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-25 13:30:08 -0500

    Update and rename cards-by-pack to cards-by-pack.php

---
+++ cards-by-pack.php
@@ -0,0 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("model-cards-by-pack.php");
+
+$PageTitle = "Cards By Pack";
+include "view-header.php";
+
+$packs = selectCardsByPack($_GET['id');
+include "view-cards-by-pack.php";
+include "view-footer.php";
+?>
