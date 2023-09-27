66aa921139819845bfe9f33f58af2cd384cc077c
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-09-27 13:30:46 -0500

    Update deck.php

--- deck.php
+++ deck.php
@@ -5,7 +5,7 @@ require_once("model-deck.php");
 $PageTitle = "Decks";
 include "view-header.php";
 
-$cards = selectDeck();
+$decks = selectDeck();
 include "view-deck.php";
 include "view-footer.php";
 ?>
