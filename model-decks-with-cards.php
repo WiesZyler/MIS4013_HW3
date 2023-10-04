3f2118143098530a0a9d2a6214d0551a3d967d1a
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-03 20:32:34 -0500

    Update model-decks-with-cards.php

--- model-decks-with-cards.php
+++ model-decks-with-cards.php
@@ -30,3 +30,20 @@ function selectPack() {
     }
 }
 ?>
+
+<?php
+function selectCardsByPack($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT P.packID, packname, packType, releasedate, cardname, cardnumber, cardtype FROM `pack` P JOIN `card` C on P.packID = C.packID where P.packID = ?");
+        $stmt->bind_param("s", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
