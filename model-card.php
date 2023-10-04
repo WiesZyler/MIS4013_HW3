f2ef2532134dc596247957303dbab827819eae99
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-04 13:47:41 -0500

    Update model-card.php

--- model-card.php
+++ model-card.php
@@ -13,3 +13,20 @@ function selectCard() {
     }
 }
 ?>
+
+<?php
+function insertCard() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `card` (`cardID`, `cardnumber`, `cardname`, `packID`, `cardtype`, `attribute`, `spell/trap`, `level`, `monstertype`, `rarity`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+         $stmt->bind_param("issssssiss",$cID,$cNum,$cName,$pID,$cType,$cAtt,$cST,$cLevel,$mType,$cRarity)
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
