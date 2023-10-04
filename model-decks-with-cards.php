95b355c26f9828a17c2cc196d0239329e7d50fb1
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-03 20:15:58 -0500

    Update model-decks-with-cards.php

--- model-decks-with-cards.php
+++ model-decks-with-cards.php
@@ -2,8 +2,24 @@
 function selectDecksWithCards($did) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT cardname, cardnumber, cardtype, d.deckID, deckname, duelistname FROM `card` c JOIN `card/deck` cd ON c.cardID = cd.cardID JOIN `deck` d ON cd.deckID = d.deckID WHERE d.deckID = ?");
+        $stmt = $conn->prepare("SELECT cardname, cardnumber, cardtype, d.deckID, deckname, duelistname, packID FROM `card` c JOIN `card/deck` cd ON c.cardID = cd.cardID JOIN `deck` d ON cd.deckID = d.deckID WHERE d.deckID = ?");
         $stmt->bind_param("i", $did);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
+
+<?php
+function selectPack() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT packID, packname, packType, releasedate FROM `pack` WHERE 1");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
