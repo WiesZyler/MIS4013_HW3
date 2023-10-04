6954baa50a43718285597eea8cf48a7aa5e7ba2f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-03 20:34:00 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -30,7 +30,7 @@ while($deck = $decks->fetch_assoc())
    <td><?php echo $deck['cardtype'];?></td>
 
  <?php
-  $packs = selectCardsByPack($deck['packID']
+  $packs = selectCardsByPack($deck['packID'])
 while($pack = $packs->fetch_assoc())
 {
 ?>
