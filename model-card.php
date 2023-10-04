82b5e404091d756db483b1833e1c052cd0cb5a53
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-04 14:12:44 -0500

    Update model-card.php

--- model-card.php
+++ model-card.php
@@ -19,7 +19,7 @@ function insertCard() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `card` (`cardID`, `cardnumber`, `cardname`, `packID`, `cardtype`, `attribute`, `spell/trap`, `level`, `monstertype`, `rarity`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-         $stmt->bind_param("issssssiss",$cID,$cNum,$cName,$pID,$cType,$cAtt,$cST,$cLevel,$mType,$cRarity)
+         $stmt->bind_param("issssssiss",$cID,$cNum,$cName,$pID,$cType,$cAtt,$cST,$cLevel,$mType,$cRarity);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
