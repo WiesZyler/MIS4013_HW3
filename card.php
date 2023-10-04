7480cdf7bfbc1533bbe97369251d7706d66bc325
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-04 14:12:05 -0500

    Update card.php

--- card.php
+++ card.php
@@ -5,6 +5,16 @@ require_once("model-card.php");
 $PageTitle = "Cards";
 include "view-header.php";
 
+if (isset($_POST['actionType']))
+{
+  switch ($_POST['actionType'])
+    {
+      case  "Add":
+      insertCard($_POST['cName'],$_POST['cNum']);
+        break;
+    }
+}
+
 $cards = selectCard();
 include "view-card.php";
 include "view-footer.php";
