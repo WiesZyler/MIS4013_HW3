cbc55607e30fe3166339ffd134795810e8fe8839
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-09 14:29:00 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -1,53 +1,41 @@
-<h1>Decks With Cards</h1>
-<?php if($deck = $decks->fetch_assoc())
-{?>
-<h2><?php echo $deck['duelistname'];?>'s <?php echo $deck['deckname'];?> Deck</h2>
- <?php
+<h1>All Decks With Cards</h1>
+<?php
+foreach ($decks as $deck) {
+?>
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</h5>
+    <p class="card-text">
+      <?php
+      $packs = selectCardsByPack($deck['packID']);
+      while ($pack = $packs->fetch_assoc()) {
+      ?>
+        <table class="table">
+          <thead>
+            <tr>
+              <th><h4><?php echo $pack['packname']; ?></h4></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $cards = selectCardsByPack($pack['packID']);
+            while ($card = $cards->fetch_assoc()) {
+            ?>
+              <tr>
+                <td><?php echo $card['cardnumber']; ?> - <?php echo $card['cardname']; ?> - <?php echo $card['cardtype']; ?></td>
+              </tr>
+            <?php
+            }
+            ?>
+          </tbody>
+        </table>
+      <?php
+      } 
+      ?>
+    </p>
+  </div>
+</div>
+<?php
 } 
 ?>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      
-      <tr>
-        <th>Card Name</th>  
-        <th>Card Number</th>
-        <th>Card Type</th>
-       <th>Pack Name</th>
-      </tr>
-      
-    </thead>
-    <tbody>
-     <?php
-
-while($deck = $decks->fetch_assoc())
-{
-?>
-<tr>
- 
-  <td><?php echo $deck['cardname'];?></td>
-  <td><?php echo $deck['cardnumber'];?></td>
-   <td><?php echo $deck['cardtype'];?></td>
- <td>
- <?php
-  $packs = selectCardsByPack($deck['packID']);
-if($pack = $packs->fetch_assoc())
-{
-?>
-
-  <?php echo $deck['packname'] . ' ';?>
-<?php
-}
- ?>
- </td>
-</tr>
-
-
-     
-
-<?php
-}
-?>
-    </tbody>
-  </table>
-</div>
