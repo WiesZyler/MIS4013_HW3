d49a6631c54380c69ca682a167ad99a0cb85ee4b
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-09 13:54:16 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -1,56 +1,6 @@
 <h1>Decks With Cards</h1>
 <?php if($deck = $decks->fetch_assoc())
 {?>
-<h2><?php echo $deck['duelistname'];?>'s <?php echo $deck['deckname'];?> Deck</h2>
- <?php
-} 
-?>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      
-      <tr>
-        <th>Card Name</th>  
-        <th>Card Number</th>
-        <th>Card Type</th>
-       <th>Pack Name</th>
-      </tr>
-      
-    </thead>
-    <tbody>
-     <?php
-
-while($deck = $decks->fetch_assoc())
-{
-?>
-<tr>
- 
-  <td><?php echo $deck['cardname'];?></td>
-  <td><?php echo $deck['cardnumber'];?></td>
-   <td><?php echo $deck['cardtype'];?></td>
- <td>
- <?php
-  $packs = selectCardsByPack($deck['packID']);
-if($pack = $packs->fetch_assoc())
-{
-?>
-
-  <?php echo $pack['packname'] . ' ';?>
-<?php
-}
- ?>
- </td>
-</tr>
-
-
-     
-
-<?php
-}
-?>
-    </tbody>
-  </table>
-</div>
 
  <?php
 while($deck = $decks->fetch_assoc())
@@ -76,7 +26,7 @@ while($deck = $decks->fetch_assoc())
    <div class="card" style="width: 18rem;">
     
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
+    <li class="list-group-item"><?php echo $deck['cardnumber'];?> - <?php echo $deck['cardname'];?> - <?php echo $deck['cardtype'];?></li>
 
 
 
@@ -84,8 +34,6 @@ while($deck = $decks->fetch_assoc())
   </ul>
 </div>
   </p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
