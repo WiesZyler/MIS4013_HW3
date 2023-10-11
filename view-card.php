5771b243c528431164deb2f625e7ef6363569e19
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-11 13:37:19 -0500

    Update view-card.php

--- view-card.php
+++ view-card.php
@@ -1,4 +1,14 @@
-<h1>Card Info</h1>
+<div class="row">
+  <div class="col">
+    <h1>Card Info</h1>
+  </div>
+    <div class="col-auto">
+        <?php
+include "view-card-newform.php"
+?>
+  </div>
+</div>
+
 <div class="table-responsive">
   <table class="table">
     <thead>
