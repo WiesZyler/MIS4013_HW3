239dab3cd515def454dc3996eb278b2240e9a2bf
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-11 13:29:44 -0500

    Update card.php

--- card.php
+++ card.php
@@ -5,6 +5,23 @@ require_once("model-card.php");
 $PageTitle = "Cards";
 include "view-header.php";
 
+if (isset($_POST['actionType']))
+{
+  switch ($_POST['actionType'])
+    {
+      case  "Add":
+      if ( insertCard($_POST['cID'],$_POST['cNum'],$_POST['cName'],$_POST['pID'],$_POST['cType'],$_POST['cAtt'],$_POST['cST'],$_POST['cLevel'],$_POST['mType'],$_POST['cRarity']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Card Added! </div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Card Not Added! </div>';
+      }
+        break;
+    }
+}
+
 $cards = selectCard();
 include "view-card.php";
 include "view-footer.php";
