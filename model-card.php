178cf9782462f7c5d790af6816824965989b8ff3
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-11 19:29:13 -0500

    Update model-card.php

--- model-card.php
+++ model-card.php
@@ -21,7 +21,7 @@ function insertCard() {
         $stmt = $conn->prepare("INSERT INTO `card` (`cardnumber`, `cardname`, `packID`, `cardtype`, `attribute`, `spell/trap`, `level`, `monstertype`, `rarity`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
          $stmt->bind_param("ssssssiss",$cNum,$cName,$pID,$cType,$cAtt,$cST,$cLevel,$mType,$cRarity);
         $stmt->execute();
-        cho "SQL Query: " . $stmt->sql;
+        echo "SQL Query: " . $stmt->sql;
         echo "Affected Rows: " . $stmt->affected_rows;
         $success = $stmt->get_result();
         $conn->close();
