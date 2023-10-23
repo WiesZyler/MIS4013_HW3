84d5124b999ce16b82c1ba4f7aa138b53605a3c7
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-23 13:42:23 -0500

    Update view-card.php

--- view-card.php
+++ view-card.php
@@ -19,6 +19,7 @@ include "view-card-newform.php"
    <th>Card Name</th>  
         <th>Card Type</th> 
       </tr>
+      <th></th>
       
     </thead>
     <tbody>
@@ -31,6 +32,14 @@ while($card = $cards->fetch_assoc())
   <td><?php echo $card['cardnumber'];?></td>
   <td><a href = "https://www.tcgplayer.com/search/all/product?q=<?php echo $card['cardname'];?>" target = "_blank"><?php echo $card['cardname'];?></td></a>
   <td><?php echo $card['cardtype'];?></td>
+  <input type="hidden" name="actiontype" value="Delete">
+  <td><button type="Submit" class="btn">Primary
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+</svg>
+    </button>
+  </td>
 
  
   
