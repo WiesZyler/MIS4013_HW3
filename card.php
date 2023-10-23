9fce00c515cf301e89973fce44d39ac4a32fab75
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-23 17:50:37 -0500

    Update card.php

--- card.php
+++ card.php
@@ -33,6 +33,16 @@ if (isset($_POST['actionType']))
         echo '<div class="alert alert-danger" role="alert"> Error! Card Not Deleted! </div>';
       }
         break;
+       case  "Edit":
+      if (editCard($_POST['cID'],$_POST['cNum'],$_POST['cName'],$_POST['pID'],$_POST['cType'],$_POST['cAtt'],$_POST['cST'],$_POST['cLevel'],$_POST['mType'],$_POST['cRarity']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Card Edited! </div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Card Not Edit! </div>';
+      }
+      break;
     }
 
 }
