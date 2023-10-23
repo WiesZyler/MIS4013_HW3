6dabfd2c4b69bff9e1cb7b26b8bd5ac90959fd70
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-23 18:32:16 -0500

    Update pack.php

--- pack.php
+++ pack.php
@@ -9,3 +9,42 @@ $packs = selectPack();
 include "view-pack.php";
 include "view-footer.php";
 ?>
+
+if (isset($_POST['actionType']))
+{
+  switch ($_POST['actionType'])
+    {
+      case  "Add":
+      if (insertCard($_POST['cNum'],$_POST['cName'],$_POST['pID'],$_POST['cType'],$_POST['cAtt'],$_POST['cST'],$_POST['cLevel'],$_POST['mType'],$_POST['cRarity']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Card Added! </div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Card Not Added! </div>';
+      }
+      break;
+      case  "Delete":
+      if (deleteCard($_POST['cID']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Card Deleted</div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Card Not Deleted! </div>';
+      }
+        break;
+       case  "Edit":
+      if (editCard($_POST['cID'],$_POST['cNum'],$_POST['cName'],$_POST['pID'],$_POST['cType'],$_POST['cAtt'],$_POST['cST'],$_POST['cLevel'],$_POST['mType'],$_POST['cRarity']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Card Edited! </div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Card Not Edit! </div>';
+      }
+      break;
+    }
+
+}
+
