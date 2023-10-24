bf4eca7092158f5fdc14155b5ca94f66ecc2657b
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 15:30:21 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -1,12 +1,12 @@
 <h1>All Decks With Cards</h1>
 <div class="card-group">
 <?php
-foreach ($decks as $deck) {
+foreach ($decks as $deck) 
+{
 ?>
   <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</h5>
-      <ul class="list-group list-group-flush">
   <table class="table">
     <thead>
       <th>Card Number</th>
@@ -16,20 +16,21 @@ foreach ($decks as $deck) {
   <tbody>
         <?php
         $cards = selectDecksWithCards($deck['deckID']);
-        while ($card = $cards->fetch_assoc()) {
+        while ($card = $cards->fetch_assoc()) 
+        {
         ?>
          
     
                <td><?php echo $card['cardnumber']; ?></td>
                <td><?php echo $card['cardname']; ?></td>
              <td><?php echo $card['quantity']; ?></td>
-            </li>
+            
             <?php
           
         } 
         ?>
   </tbody>
-      </ul>
+    
     </div>
   </div>
 <?php
