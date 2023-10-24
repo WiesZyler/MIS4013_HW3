82781931ea821f4c7f67362db85cbc494ef0d4be
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 17:39:31 -0500

    Update view-decks-with-cards-editform.php

--- view-decks-with-cards-editform.php
+++ view-decks-with-cards-editform.php
@@ -28,6 +28,7 @@
                    
                     <input type="hidden" name="cID" value="<?php echo $card['cardID'];?>">
                       <input type="hidden" name="dID" value="<?php echo $card['deckID'];?>">
+                   <input type="hidden" name="dID" value="<?php echo $card['carddeckID'];?>">
                     <input type="hidden" name="actionType" value="Edit">
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
