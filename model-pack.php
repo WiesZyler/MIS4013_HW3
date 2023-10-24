79842f8719ab39bb9718dc534680c6f92ef989bf
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-23 19:32:14 -0500

    Update model-pack.php

--- model-pack.php
+++ model-pack.php
@@ -46,7 +46,7 @@ function deletePack($pID) {
     }
 }
 
-function editPack($pName,$pType,$rDate) {
+function editPack($pName,$pType,$rDate $pID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("UPDATE `pack` SET `packname`=?, `packType`=?, `releasedate`=? WHERE `packID`=?");
@@ -60,6 +60,5 @@ function editPack($pName,$pType,$rDate) {
         throw $e;
     }
 }
-
 
 ?>
