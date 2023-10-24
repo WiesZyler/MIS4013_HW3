15ee277aab0c1366e6613e7767e7aff88819ee20
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 16:46:48 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -1,45 +1,27 @@
 <h1>All Decks With Cards</h1>
-  <div class="card-group">
+<div class="card-group">
 <?php
-while ($deck = $decks->fetch_assoc()){
-  ?>
+while ($deck = $decks->fetch_assoc()) {
+?>
   <div class="card">
     <div class="card-body">
       <h5 class="card-title"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</h5>
       <p class="card-text">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Card Number</th>
-            <th>Card Name</th>
-            <th> Quantity</th>
-      </tr>
-      
-    </thead>
-  <tbody>
-        <?php
-        $cards = selectDecksWithCards($deck['deckID']);
-        while ($card = $cards->fetch_assoc()) 
-        {
-        ?>
-         
-              <tr>
-               <td><?php echo $card['cardnumber']; ?></td>
-               <td><?php echo $card['cardname']; ?></td>
-             <td><?php echo $card['quantity']; ?></td>
-                </tr>
-            
-            <?php
-          
-        } 
-        ?>
-  </tbody>
-    </p>
-    
+      <ul class="list-group">
+<?php
+$cards = selectDecksWithCards($deck['deckID']);
+  while ($card = $cards->fetch_assoc()) {
+?>
+    <li class="list-group-item"><?php echo $card['cardnumber']; ?> - <?php echo $card['cardname']; ?>  -<?php echo $card['quantity']; ?></li>
+<?php
+  }
+?>
+      </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor['office_number']; ?></small></p>
     </div>
   </div>
-   
 <?php
-} 
+}
 ?>
- </div>
+</div>
