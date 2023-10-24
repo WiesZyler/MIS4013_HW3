3ca9a0ab7fa7b972762e3558acb3fab1aaaa72a1
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 15:37:34 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -1,50 +1,43 @@
 <h1>All Decks With Cards</h1>
 <div class="card-group">
 <?php
-foreach ($duelists as $duelist) {
+foreach ($decks as $deck) 
+{
 ?>
   <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title"><?php echo $duelist['duelistname']; ?></h5>
-    </div>
-    <?php
-    foreach ($decks as $deck) {
-      if ($deck['duelistname'] === $duelist['duelistname']) {
-    ?>
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title"><?php echo $deck['deckname']; ?> Deck</h5>
-          <table class="table">
-            <thead>
+      <h5 class="card-title"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</h5>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Card Number</th>
+            <th>Card Name</th>
+            <th> Quantity</th>
+      </tr>
+      
+    </thead>
+  <tbody>
+        <?php
+        $cards = selectDecksWithCards($deck['deckID']);
+        while ($card = $cards->fetch_assoc()) 
+        {
+        ?>
+         
               <tr>
-                <th>Card Number</th>
-                <th>Card Name</th>
-                <th>Quantity</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              $cards = selectDecksWithCards($deck['deckID']);
-              while ($card = $cards->fetch_assoc()) {
-              ?>
-                <tr>
-                  <td><?php echo $card['cardnumber']; ?></td>
-                  <td><?php echo $card['cardname']; ?></td>
-                  <td><?php echo $card['quantity']; ?></td>
+               <td><?php echo $card['cardnumber']; ?></td>
+               <td><?php echo $card['cardname']; ?></td>
+             <td><?php echo $card['quantity']; ?></td>
                 </tr>
-              <?php
-              }
-              ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    <?php
-      }
-    }
-    ?>
+            
+            <?php
+          
+        } 
+        ?>
+  </tbody>
+    
+    </div>
   </div>
 <?php
-}
+} 
 ?>
 </div>
