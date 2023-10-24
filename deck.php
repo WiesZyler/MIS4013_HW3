ff06c94ef10d5aad9c671e9b95aec6e5f95287d8
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-23 20:38:20 -0500

    Update deck.php

--- deck.php
+++ deck.php
@@ -5,6 +5,45 @@ require_once("model-deck.php");
 $PageTitle = "Decks";
 include "view-header.php";
 
+if (isset($_POST['actionType']))
+{
+  switch ($_POST['actionType'])
+    {
+      case  "Add":
+      if (insertCard($_POST['deckName'],$_POST['duelistName']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Deck Added! </div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Deck Not Added! </div>';
+      }
+      break;
+      case  "Delete":
+      if (deleteCard($_POST['dID']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Deck Deleted</div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Deck Not Deleted! </div>';
+      }
+        break;
+       case  "Edit":
+      if (editCard($_POST['deckName'],$_POST['duelistName'],$_POST['dID']))
+      {
+       echo '<div class="alert alert-success" role="alert"> Deck Edited! </div>';
+          }
+      else
+      {
+        echo '<div class="alert alert-danger" role="alert"> Error! Deck Not Edited! </div>';
+      }
+      break;
+    }
+
+}
+
+
 $decks = selectDeck();
 include "view-deck.php";
 include "view-footer.php";
