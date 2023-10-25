4ac845a88ca6c24852edba985c5808a9d331522a
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 19:06:52 -0500

    Update view-deck-input-list.php

--- view-deck-input-list.php
+++ view-deck-input-list.php
@@ -2,7 +2,7 @@
   <?php
 while ($deckItem = $deckList->fetch_assoc()){
   ?>
-<option value="<?php echo deckItem['deckID'];?>"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</option>
+<option value="<?php echo $deckItem['deckID'];?>"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</option>
 
   <?php
 }
