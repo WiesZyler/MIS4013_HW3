ff40acf3b8ec44210f0ed0678b1008d9b021a067
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 20:36:19 -0500

    Update view-decks-with-cards-editform.php

--- view-decks-with-cards-editform.php
+++ view-decks-with-cards-editform.php
@@ -2,7 +2,7 @@
 
 
 <!-- Button trigger modal -->
-<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editCardModal<?php echo $card['deckID'];?>">
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editCardModal<?php echo $card['carddeckID'];?>">
  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -10,11 +10,11 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="editCardModal<?php echo $card['deckID'];?>" tabindex="-1" aria-labelledby="editCardModalLabel<?php echo $card['deckID'];?>" aria-hidden="true">
+<div class="modal fade" id="editCardModal<?php echo $card['carddeckID'];?>" tabindex="-1" aria-labelledby="editCardModalLabel<?php echo $card['carddeckID'];?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editCardModalLabel<?php echo $card['deckID'];?>">Edit Quantity</h1>
+        <h1 class="modal-title fs-5" id="editCardModalLabel<?php echo $card['carddeckID'];?>">Edit Quantity</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -22,7 +22,7 @@
 
 
                    <div class="mb-3">
-                      <label for="dID<?php echo $card['deckID'];?>" class="form-label">Deck</label>
+                      <label for="dID<?php echo $card['carddeckID'];?>" class="form-label">Deck</label>
                       <?php 
                       $deckList = selectDecksforInput();
                       $selectedDeck = $card['deckID'];
@@ -31,13 +31,13 @@
                     </div>
                     
                     <div class="mb-3">
-                      <label for="cID<?php echo $card['deckID'];?>" class="form-label">Card ID </label>
-                      <input type="text" class="form-control" id="cID<?php echo $card['deckID'];?>" name="cID" value="<?php echo $card['cardID'];?>">
+                      <label for="cID<?php echo $card['carddeckID'];?>" class="form-label">Card ID </label>
+                      <input type="text" class="form-control" id="cID<?php echo $card['carddeckID'];?>" name="cID" value="<?php echo $card['cardID'];?>">
                     </div>
        
                   <form method="post" action="">         
                     <div class="mb-3">
-                      <label for="cdQ<?php echo $card['deckID'];?>" class="form-label">Quantity</label>
+                      <label for="cdQ<?php echo $card['carddeckID'];?>" class="form-label">Quantity</label>
                       <input type="text" class="form-control" id="cdQ<?php echo $card['deckID'];?>"  name="cdQ" value="<?php echo $card['quantity'];?>">
                     </div>
 
