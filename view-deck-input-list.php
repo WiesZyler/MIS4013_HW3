9ad50b403355faa60c342161e17dd64d97760e7c
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 19:05:38 -0500

    Update view-deck-input-list.php

--- view-deck-input-list.php
+++ view-deck-input-list.php
@@ -1,6 +1,6 @@
 <select class="form-select" id="did" name="did">
   <?php
-while ($deckItem = $deckList->fetch->fetch_assoc()){
+while ($deckItem = $deckList->fetch_assoc()){
   ?>
 <option value="<?php echo deckItem['deckID'];?>"><?php echo $deck['duelistname']; ?>'s <?php echo $deck['deckname']; ?> Deck</option>
 
