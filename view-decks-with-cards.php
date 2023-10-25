9f2ab90cc6de48ecabfec32c62285eb44a79a73e
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-25 14:02:28 -0500

    Update view-decks-with-cards.php

--- view-decks-with-cards.php
+++ view-decks-with-cards.php
@@ -8,7 +8,7 @@ include "view-decks-with-cards-newform.php";
 ?>
   </div>
 </div>
-<div class="row row-cols-1 row-cols-md-2 g-4">
+<div class="row row-cols-1 row-cols-md-2 g-4" style="margin-left:10px;">
 <?php
 while ($deck = $decks->fetch_assoc()) {
 ?>
