b70199b8e37993291942bd0d6dddf2c292db9e3b
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 21:42:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "view-header.php";
 ?>
     
 <head>
-    <meta charset="UTF-8">
+        <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Yu-Gi-Oh Database</title>
