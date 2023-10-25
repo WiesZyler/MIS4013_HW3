c5cda3131e97f23a67c929240bc43c0de1131ed2
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 21:32:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,31 @@
 $PageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Homework 3</h1>
+    <h1>Homework 4</h1>
+<head>
+    <title>Yu-Gi-Oh Database</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to the Yu-Gi-Oh Database</h1>
+        <p>Your source for all things Yu-Gi-Oh!</p>
+    </header>
+   
+    <main>
+        <section class="featured-cards">
+            <h2>Featured Cards</h2>
+            <!-- Add card images and information here -->
+        </section>
+        <section class="popular-decks">
+            <h2>Popular Decks</h2>
+            <!-- Add deck information and images here -->
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Yu-Gi-Oh Database. All rights reserved.</p>
+    </footer>
+</body>
+</html>
 
 <?php
 include "view-footer.php";
