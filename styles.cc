1c2c4283fa999027290861e2cb8aeb1ec351e046
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 21:35:30 -0500

    Create styles.cc

---
+++ styles.cc
@@ -0,0 +1,21 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #1f1f1f;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    text-align: center;
+    background-color: #f44336;
+    padding: 20px;
+}
+
+main {
+    padding: 20px;
+}
+
+h2 {
+    color: #f44336;
+}
