3f58fc243ffedf1f6c070666c1a9976ee4ac4efa
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-24 20:42:22 -0500

    Update model-decks-with-cards.php

--- model-decks-with-cards.php
+++ model-decks-with-cards.php
@@ -60,11 +60,11 @@ function deleteCard($cdID) {
     }
 }
 
-function editCard($cdQ, $cdID, $cID, $dID) {
+function editCard($dID, $cID, $cdQ, $cdID) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `card/deck` SET `quantity`=? WHERE `carddeckID`=? AND `cardID`=? AND `deckID`=?");
-        $stmt->bind_param("iiii", $cdQ, $cdID, $cID, $dID);  
+        $stmt = $conn->prepare("UPDATE `card/deck` SET  `deckID`=?, `cardID`=?, `quantity`=? WHERE `carddeckID`=?");
+        $stmt->bind_param("iiii", $dID, $cID, $cdQ, $cdID, );  
         $success = $stmt->execute();  
         $conn->close();
         return $success;
