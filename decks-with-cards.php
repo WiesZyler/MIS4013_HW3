1690250980e28fc6c8c1a109d3f2dba1ddc026e7
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-10-30 14:03:22 -0500

    Update decks-with-cards.php

--- decks-with-cards.php
+++ decks-with-cards.php
@@ -7,7 +7,7 @@ require_once("util-db.php");
 require_once("model-decks-with-cards.php");
 
 $PageTitle = "Decks With Cards";
-include "view-header.php";
+include "view/header.php";
 
 if (isset($_POST['actionType']))
 {
