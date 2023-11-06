efe1a3eb6feaf1ced0794f920e484bc1ec797efe
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-06 13:52:12 -0600

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -5,13 +5,34 @@ include "view/header.php";
 
 <head>
       <h1>Javascript Examples</h1>
+        <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- charts -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <!-- Data table -->
+    <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
+    <script src="https://unpkg.com/gridjs/dist/gridjs.umd.js"></script>
+
+    <!-- Animation -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+
+    <!-- Map -->
+    <link href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" rel="stylesheet" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+    <!-- sweetalert2 -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.8.0/sweetalert2.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.8.0/sweetalert2.all.min.js"></script>
 </head>
 <body>
 <div>
    <button id="btn" class="btn btn-primary">Click Here</button>
     <div id="d1" style="width:100px; height:100px; background-color:red; border-style:solid;">0-0</div>
-    var g;
     <script>
+    
         "use strict";
         {
             let exBtn = document.querySelector('#btn');
