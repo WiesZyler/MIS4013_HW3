021c26777c32555f237b53f045325fb9e43ad78d
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-06 13:44:37 -0600

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -2,13 +2,38 @@
 $PageTitle = "Home";
 include "view/header.php";
 ?>
-    
+
 <head>
       <h1>Javascript Examples</h1>
 </head>
 <body>
+<div>
+   <button id="btn">Click Here</button>
+    <div id="d1" style="width:100px; height:100px; background-color:red; border-style:solid;></div>
+    var g;
+    <script>
+        "use strict";
+        {
+            let exBtn = document.querySelector('#btn');
+            exBtn.AddEventListener("click", () => {
+
+            let div = document.querySelector("#d1");
+            let params = {
+                        targets: div,
+                        translateX: { value: 200, duration: 3000 }
+                         };
+                    anime(params);
+
+        
+            })
+        }
+        
+    </script> 
+</div>
+
 
 </body>
 <?php
 include "view/footer.php";
 ?>
+
