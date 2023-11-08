f8312ba69931e377351dc153a7e863ce80ebfbdd
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-08 14:24:54 -0600

    Update card-table.php

--- card-table.php
+++ card-table.php
@@ -1,14 +1,11 @@
 <?php 
+$PageTitle = "CardTable";
+include "view/header.php";
+
 require_once("util-db.php");
 require_once("model-card.php");
 ?>
-<div class="row">
-  <div class="col">
-    <h1>Card Info</h1>
-  </div>
-    <div class="col-auto">
-        <?php
-include "view-card-newform.php"
+
 ?>
   </div>
 </div>
