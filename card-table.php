efc296d62bfc27ed460e7a2578dde1215c37f154
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-08 14:23:14 -0600

    Update card-table.php

--- card-table.php
+++ card-table.php
@@ -12,4 +12,39 @@ include "view-card-newform.php"
 ?>
   </div>
 </div>
+<div id="table"></div>
+	<script>
+		"use strict";
+		var grid = null; 
+		var g;
+
+		async function ShowTable() {
+			if (grid != null) {
+				grid.destroy
+			}
+
+			let table = document.querySelector("#table");
+			table.innerHTML = ""; 
+
+			let r = await fetch(selectCard(), { cache: "no-store" });
+			let rj = await r.json();
+
+			g = rj;
+
+			let params = {
+				data: rj,
+				pagination: { limit: 10, },
+				search: true,
+				sort: true,
+				width: 600,
+
+			}
+
+			grid = new gridjs.Grid(params);
+			grid.render(table);
+
+			
+		}
+		ShowTable();
+	</script>
 
