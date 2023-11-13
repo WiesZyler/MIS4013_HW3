c76b1f8ddac64533b3eee2ed3541ce9c85d0c6d5
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-13 14:23:21 -0600

    Update card-table.php

--- card-table.php
+++ card-table.php
@@ -1,54 +1,36 @@
 <?php 
 $PageTitle = "CardTable";
 include "view/header.php";
-
-require_once("util-db.php");
-require_once("model-card.php");
 ?>
 
 <div id="table"></div>
-	<script>
-		"use strict";
-		var grid = null; 
-		var g;
+<script>
+    "use strict";
+    var grid = null; 
 
-async function ShowTable() {
-    if (grid != null) {
-        grid.destroy();
-    }
-    let table = document.querySelector("#table");
-    table.innerHTML = "";
-
-    try {
-        let r = await fetch("
-<?php
-$cards = selectCard(); 
-$cardData = array();
-
-while ($card = $cards->fetch_assoc()) {
-    $cardData[] = $card;
-}
-
-echo json_encode($cardData);
-?>
-
-", { cache: "no-store" });
-        let rj = await r.json();
-
-
-        let params = {
-            data: rj,
-            pagination: { limit: 10 },
-            search: true,
-            sort: true,
-            width: 600,
-            // columns: [{ id: "cardID", name: "Card ID" }, { id: "cardnumber", name: "Card Number" }, { id: "cardname", name: "Card Name" },{ id: "packID", name: "Pack ID" },]
+    async function ShowTable() {
+        if (grid != null) {
+            grid.destroy();
         }
+        let table = document.querySelector("#table");
+        table.innerHTML = "";
 
-        grid = new gridjs.Grid(params);
-        grid.render(table);
-}
+        try {
+            let r = await fetch("getCardData.php", { cache: "no-store" });
+            let rj = await r.json();
 
-ShowTable();
+            let params = {
+                data: rj,
+                pagination: { limit: 10 },
+                search: true,
+                sort: true,
+                width: 600,
+            }
 
-	</script>
+            grid = new gridjs.Grid(params);
+            grid.render(table);
+       
+    }
+
+    ShowTable();
+</script>
