1ecd6ea81ce4588adcd3fdae1b3a30d1bb8c4281
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-14 19:16:03 -0600

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -130,6 +130,7 @@ include "view/header.php";
       <div style="margin-top:4000px;"></div>
        <p style="font-weight:bold;">Uh Oh, looks like you're on the bottom floor!</p>
       <p style="font-weight:bold;">Just press that up arrow to take the elevator back up!</p>
+      <p style="font-weight:bold;">Oh, and remember to have your volume up to hear that classic music!</p>
       <div id="elevator" class="elevator-button btn" style="margin-right:auto; margin-left:auto; display:flex; align-itmes:center; justify-content:center;">
              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
