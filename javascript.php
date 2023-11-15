53f9347d183144425242382a39388bae131ab22f
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-15 13:31:44 -0600

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -142,7 +142,7 @@ include "view/header.php";
         element: document.querySelector('#elevator'),
         mainAudio: 'elevator/elevator.mp3', // Optional: Specify a sound file
         endAudio: 'elevator/ding.mp3', // Optional: Specify a sound file for the end
-        duration: 40000,
+        duration: 45000,
     });
 </script>
 
