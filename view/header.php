135a4cc7a66e68aa17db3d7fef267113fe34add9
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-14 18:42:54 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -21,7 +21,7 @@
 
   <!-- Elevator.css -->
     <script type="text/javascript" src="elevator.js"></script>
-    <script type="module" src="your-script.js"></script>
+    <script type="module" src="elevator.js"></script>
     
     
     <!-- Map -->
