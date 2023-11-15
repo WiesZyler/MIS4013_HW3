c87e2a3f0cfe0acea1ecff436cc678c0b28cfcbe
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-14 19:06:43 -0600

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -37,9 +37,7 @@ include "view/header.php";
       </div>
       <p>Hover over the block to rotate it!</p>
     <div id="d1" style="width:100px; height:100px; background-color:red; border-style:solid; display:flex; flex-direction:column; justify-content:center; align-item:center; margin-left:auto; margin-right:auto;"></div>
-     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
-       <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-      </svg>
+    
       <script>
     
         "use strict";
@@ -132,6 +130,9 @@ include "view/header.php";
       <div style="height:4000px;"></div>
       <div id="elevator" class="elevator-button btn btn-primary" style="margin-right:auto; margin-left:auto;">
         <p style="font-size: 40px;">Back to the Top</p>
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+       <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+      </svg>
        </div>
       <script>
           
