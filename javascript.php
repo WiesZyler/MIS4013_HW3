f63f376ea62c9c5499b3172a407e6541d1d49659
WiesZyler <122990301+WiesZyler@users.noreply.github.com>
2023-11-14 19:09:00 -0600

    Update javascript.php

--- javascript.php
+++ javascript.php
@@ -128,7 +128,7 @@ include "view/header.php";
     
       <p style="font-weight:bold;">Psst, Scroll all the away to the bottom, do it!</p>
       <div style="height:4000px;"></div>
-      <div id="elevator" class="elevator-button btn btn-primary" style="margin-right:auto; margin-left:auto; display:flex; align-itmes:center; justify-content:center;">
+      <div id="elevator" class="elevator-button btn" style="margin-right:auto; margin-left:auto; display:flex; align-itmes:center; justify-content:center;">
              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
       </svg>
